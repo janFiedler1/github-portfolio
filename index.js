1430e76f8d951940cf04b6096d4471fe4702c158
janFiedler1 <50682895+janFiedler1@users.noreply.github.com>
2024-03-02 23:43:27 -0500

    content, random color functions

--- index.js
+++ index.js
@@ -11,3 +11,41 @@ function night_mode() {
   }
 }
 profile_pic.addEventListener("click", night_mode);
+
+
+
+function randomColors() {
+  var colors = ['#ff0000', '#00ff00', '#0000ff'];
+
+  let spans = document.getElementsByTagName('span');
+  for (let index = 0; index < spans.length; index++) {
+    //let random_color = colors[Math.floor(Math.random() * colors.length)];
+    let random_color = "#"+(Math.floor(Math.random() * 16777216)).toString(16);
+    spans[index].style.backgroundColor = random_color;
+    spans[index].style.color = contrastingColor(random_color);
+}
+  
+}
+
+function contrastingColor(color)
+{
+    return (luma(color) >= 165) ? '000' : 'fff';
+}
+function luma(color) // color can be a hx string or an array of RGB values 0-255
+{
+  console.log(color);
+  console.log(hexToRGBArray(color));
+    var rgb = (typeof color === 'string') ? hexToRGBArray(color.slice(0,6)) : color;
+    return (0.2126 * rgb[0]) + (0.7152 * rgb[1]) + (0.0722 * rgb[2]); // SMPTE C, Rec. 709 weightings
+}
+function hexToRGBArray(color)
+{
+    if (color.length === 3)
+        color = color.charAt(0) + color.charAt(0) + color.charAt(1) + color.charAt(1) + color.charAt(2) + color.charAt(2);
+    else if (color.length !== 6)
+        throw('Invalid hex color: ' + color);
+    var rgb = [];
+    for (var i = 0; i <= 2; i++)
+        rgb[i] = parseInt(color.substr(i * 2, 2), 16);
+    return rgb;
+}
